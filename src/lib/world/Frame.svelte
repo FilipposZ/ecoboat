d166b41348278d48c3224208eeb599678c9abb8e
Filippos Zacharopoulos <fi.zacharop@gmail.com>
2025-01-03 00:46:26 +0100

    add text options and better theme management

--- src/lib/world/Frame.svelte
+++ src/lib/world/Frame.svelte
@@ -2,7 +2,7 @@
 	import { Plane, Vector3 } from 'three';
 	import { useThrelte } from '@threlte/core';
 	import { gsap } from 'gsap';
-	import { Folder, Text as PaneText, Slider, Point } from 'svelte-tweakpane-ui';
+	import { Folder, Text as PaneText, Slider, Point, Textarea, Checkbox } from 'svelte-tweakpane-ui';
 	import { Text, TransformControls } from '@threlte/extras';
 	import { configPane } from '$lib/configuration/config.svelte';
 
@@ -10,38 +10,88 @@
 
 	configPane.addConfigTabPage(frameConfigTabPage);
 
+	import { DEG2RAD } from 'three/src/math/MathUtils.js';
+	class TextOptions {
+		value: string = $state('A trip with the boat ..');
+		position: Vector3 = $state(new Vector3(0, 9.2, 3.4));
+		size: number = $state(0.2);
+
+		opacity = $state(0);
+		bevelEnabled = $state(true);
+		bevelOffset = $state(0);
+		bevelSegments = $state(20);
+		bevelSize = $state(0.2);
+		bevelThickness = $state(0.1);
+		curveSegments = $state(12);
+		height = $state(1);
+		smooth = $state(0.1);
+	}
+
 	export class Frame {
 		cameraPosition: Vector3 = $state(new Vector3(3.2, 8, 7));
-		text: string = $state('A trip with the boat ..');
-		textPos: Vector3 = $state(new Vector3(0, 9.2, 3.4));
-		fontSize: number = $state(0.2);
+		text: TextOptions = $state(new TextOptions());
 	}
 
 	const frame = $derived(new Frame());
+	let timeline: gsap.core.Timeline;
 
 	$effect(() => {
-		let timeline = gsap.timeline().to(camera.current.position, {
-			x: frame.cameraPosition.x,
-			y: frame.cameraPosition.y,
-			z: frame.cameraPosition.z,
-			duration: 5,
-			ease: 'expo.out'
-		});
+		timeline = gsap
+			.timeline()
+			.to(camera.current.position, {
+				x: frame.cameraPosition.x,
+				y: frame.cameraPosition.y,
+				z: frame.cameraPosition.z,
+				duration: 3,
+				ease: 'expo.out'
+			})
+			.to(
+				frame.text,
+				{
+					opacity: 1,
+					duration: 10,
+					ease: 'back'
+				},
+				'>-2'
+			);
 	});
 </script>
 
-<TransformControls>
-	<Text
-		text={frame.text}
-		fontSize={frame.fontSize}
-		position={[frame.textPos.x, frame.textPos.y, frame.textPos.z]}
-	/>
-</TransformControls>
+<!-- <TransformControls> -->
+<Text
+	text={frame.text.value}
+	fillOpacity={frame.text.opacity}
+	fontSize={frame.text.size}
+	position={[frame.text.position.x, frame.text.position.y, frame.text.position.z]}
+/>
+<!-- </TransformControls> -->
 
 {#snippet frameConfigTabPage()}
 	<Folder title="Text" expanded={false}>
-		<PaneText bind:value={frame.text} />
-		<Slider bind:value={frame.fontSize} />
-		<Point bind:value={frame.textPos} label="Position" expanded={true} picker="inline" />
+		<Textarea bind:value={frame.text.value} label="Text" />
+		<Slider label="Font Size" bind:value={frame.text.size} min={0} max={10} />
+		<Point bind:value={frame.text.position} label="Position" expanded={true} picker="inline" />
+		<Folder title="Advanced Options" expanded={false}>
+			<Checkbox label="bevelEnabled" bind:value={frame.text.bevelEnabled} />
+			<Slider label="bevelOffset" bind:value={frame.text.bevelOffset} min={0} max={2} />
+			<Slider
+				label="bevelSegments"
+				bind:value={frame.text.bevelSegments}
+				step={1}
+				min={0}
+				max={50}
+			/>
+			<Slider label="bevelSize" bind:value={frame.text.bevelSize} min={0} max={2} />
+			<Slider label="bevelThickness" bind:value={frame.text.bevelThickness} min={0} max={2} />
+			<Slider
+				label="curveSegments"
+				bind:value={frame.text.curveSegments}
+				step={1}
+				min={0}
+				max={50}
+			/>
+			<Slider label="height" bind:value={frame.text.height} min={0} max={5} />
+			<Slider label="smooth" bind:value={frame.text.smooth} min={0} max={180 * DEG2RAD} />
+		</Folder>
 	</Folder>
 {/snippet}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const config = {
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	content: ['./src/**/*.{svelte,js,ts,html}'],
 	daisyui: {
-		themes: ["halloween", "fantasy"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: ["fantasy", "halloween"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: 'dark', // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
