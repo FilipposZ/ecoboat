cf433a9c360847854d74aef31fdf79e89970116d
Filippos ZACHAROPOULOS <filippos.zacharopoulos@amadeus.com>
2025-01-03 17:12:13 +0100

    make the environment responsive to the selected theme

---
+++ src/lib/patterns/observer.svelte.ts
@@ -0,0 +1,45 @@
+export interface Subject {
+    attach(observer: Observer): void;
+    detach(observer: Observer): void;
+    notify(): void;
+}
+
+export class Subject {
+    private observers: Observer[] = [];
+
+    public attach(observer: Observer): void {
+        console.log(this.observers);
+        const isExist = this.observers.includes(observer);
+        if (isExist) {
+            return console.log(`${this.constructor.name}: The ${observer.constructor.name} has been attached already.`);
+        }
+
+        console.log(`${this.constructor.name}: Attached the observer ${observer.constructor.name}.`);
+        this.observers.push(observer);
+    }
+
+    public detach(observer: Observer): void {
+        const observerIndex = this.observers.indexOf(observer);
+        if (observerIndex === -1) {
+            return console.log(`${this.constructor.name}: Nonexistent observer ${observer.constructor.name}.`);
+        }
+
+        this.observers.splice(observerIndex, 1);
+        console.log(`${this.constructor.name}: Detached the observer ${observer.constructor.name}.`);
+    }
+
+    public notify(): void {
+        if (!this.observers?.length) {
+            return;
+        }
+        console.log(`${this.constructor.name}: Notifying the observers: ${this.observers.map(observer => observer.constructor.name)}`);
+        for (const observer of this.observers) {
+            observer.update(this);
+        }
+    }
+}
+
+export interface Observer {
+    // Receive update from subject.
+    update(subject: Subject): void;
+}
