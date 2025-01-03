f5e4e36f8b8c076dfa506c037ef4d037119f31f5
Filippos ZACHAROPOULOS <filippos.zacharopoulos@amadeus.com>
2025-01-03 17:53:19 +0100

    update dependencies

--- src/routes/stripe/stripe.ts
+++ src/routes/stripe/stripe.ts
@@ -3,6 +3,6 @@ import { STRIPE_SECRET_KEY } from '$env/static/private';
 
 //TODO: Update to latest stripe client but seems to cause error
 export const stripeClient = new Stripe(STRIPE_SECRET_KEY, {
-	apiVersion: '2024-06-20',
+	apiVersion: '2024-12-18.acacia',
 	httpClient: Stripe.createFetchHttpClient()
 });
