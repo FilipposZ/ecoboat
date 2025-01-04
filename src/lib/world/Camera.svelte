c395cd3ab6db09235299e1bae369a5b2f58dc295
Filippos Zacharopoulos <fi.zacharop@gmail.com>
2025-01-04 20:16:11 +0100

    rename addConfigTabPage to addConfigSnippet

--- src/lib/world/Camera.svelte
+++ src/lib/world/Camera.svelte
@@ -9,24 +9,18 @@
 
 	const { camera } = useThrelte();
 
-	configPane.addConfigTabPage(cameraConfigPage);
+	configPane.addConfigSnippet(cameraConfigPage);
 
-	let cameraState: Transform = $state({
-		position: { x: 3.5, y: 8.5, z: 10 },
-		rotation: { x: 0, y: 0, z: 0 }
+	let cameraState: { current: Transform; target: Transform } = $state({
+		current: { position: { x: 3.5, y: 8.5, z: 10 }, rotation: { x: 0, y: 0, z: 0 } },
+		target: { position: { x: 3.5, y: 8.5, z: 10 }, rotation: { x: 0, y: 0, z: 0 } }
 	});
 
 	let shouldForceCameraState = $state(false);
 
-	let cameraRotation = $state({
-		current: cameraState.rotation,
-		target: cameraState.rotation,
-		ease: 0.2
-	});
-
 	function resetCameraState() {
-		cameraState.position = { x: 3.5, y: 8.5, z: 8 };
-		cameraState.rotation = { x: 0, y: 0, z: 0 };
+		cameraState.target.position = { x: 3.5, y: 8.5, z: 8 };
+		cameraState.target.rotation = { x: 0, y: 0, z: 0 };
 	}
 
 	onMount(() => {
@@ -42,33 +36,32 @@
 		const normalizedPosY = (event.y / window.innerHeight) * 2 - 1;
 
 		// console.log(normalizedPosX, normalizedPosY);
-		cameraRotation.target = {
+		cameraState.target.rotation = {
 			x: -normalizedPosY * 0.1,
 			y: -normalizedPosX * 0.1,
 			z: camera.current.rotation.z
 		};
 	}
 
-	function applyMouseCameraRotation() {
-		cameraRotation.current = gsap.utils.interpolate(
-			cameraRotation.current,
-			cameraRotation.target,
-			cameraRotation.ease
-		);
-		cameraState.rotation = cameraRotation.current;
-	}
-
 	useTask(() => {
 		if (shouldForceCameraState) {
-			applyMouseCameraRotation();
+			cameraState.current = gsap.utils.interpolate(cameraState.current, cameraState.target, 0.1);
 		}
 	});
 </script>
 
 <T.PerspectiveCamera
 	makeDefault
-	position={[cameraState.position.x, cameraState.position.y, cameraState.position.z]}
-	rotation={[cameraState.rotation.x, cameraState.rotation.y, cameraState.rotation.z]}
+	position={[
+		cameraState.current.position.x,
+		cameraState.current.position.y,
+		cameraState.current.position.z
+	]}
+	rotation={[
+		cameraState.current.rotation.x,
+		cameraState.current.rotation.y,
+		cameraState.current.rotation.z
+	]}
 	fov={70}
 >
 	<!-- <OrbitControls enableZoom={true} /> -->
@@ -77,9 +70,9 @@
 {#snippet cameraConfigPage()}
 	<Folder title="Camera" expanded={false}>
 		<Checkbox bind:value={shouldForceCameraState} label="Move camera with mouse" />
-		<Point bind:value={cameraState.position} label="Position" picker="inline" />
+		<Point bind:value={cameraState.target.position} label="Position" picker="inline" />
 		<RotationEuler
-			bind:value={cameraState.rotation}
+			bind:value={cameraState.target.rotation}
 			expanded={true}
 			label="Rotation"
 			picker={'inline'}
