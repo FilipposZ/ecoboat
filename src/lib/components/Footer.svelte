47734451c534349e1200015f3562a4bcb0b5c33f
Filippos Zacharopoulos <fi.zacharop@gmail.com>
2025-02-08 11:23:43 +0100

    fix footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,28 +4,27 @@
 	import SocialIcons from './SocialIcons.svelte';
 </script>
 
-<div class="bg-base-100 relative z-50 mt-32 w-full">
-	<footer class="footer p-10 text-base-content justify-items-center">
-		<aside>
-			<div class="flex items-center h-fit space-x-2">
-				<Ship class="h-8 w-8" />
-				<div class="text-xl font-bold">{PUBLIC_PROJECT_NAME}</div>
-			</div>
-		</aside>
-		<SocialIcons class="md:hidden" />
-
-		<nav class="justify-items-center md:justify-items-start">
-			<h6 class="footer-title">LINKS</h6>
-			<a class="link link-hover" href="/">Home</a>
-			<a class="link link-hover" href="/#about">About</a>
-		</nav>
-		<nav class="justify-items-center md:justify-items-start">
-			<h6 class="footer-title">Legal</h6>
-			<a class="link link-hover" href="/terms-of-use">Terms of use</a>
-			<a class="link link-hover" href="/privacy-policy">Privacy policy</a>
-			<a class="link link-hover" href="mailto:someone@example.com">Contact</a>
-		</nav>
-
-		<SocialIcons class="hidden md:block" />
-	</footer>
-</div>
+<footer class="footer sm:footer-horizontal relative bg-base-100 text-base-content p-10 mt-15 z-50">
+	<aside>
+		<Ship class="h-8 w-8" />
+		<h2>
+			{PUBLIC_PROJECT_NAME}
+		</h2>
+		<p>The eco-friendly boat museum</p>
+	</aside>
+	<nav>
+		<h6 class="footer-title">Find us at</h6>
+		<SocialIcons />
+	</nav>
+	<nav>
+		<h6 class="footer-title">Links</h6>
+		<a class="link link-hover" href="/">Home</a>
+		<a class="link link-hover" href="/#about">About us</a>
+	</nav>
+	<nav>
+		<h6 class="footer-title">Legal</h6>
+		<a class="link link-hover" href="/terms-of-use">Terms of use</a>
+		<a class="link link-hover" href="/privacy-policy">Privacy policy</a>
+		<a class="link link-hover" href="mailto:someone@example.com">Contact</a>
+	</nav>
+</footer>
--- src/lib/components/SocialIcons.svelte
+++ src/lib/components/SocialIcons.svelte
@@ -3,6 +3,5 @@
 </script>
 
 <div class="justify-items-center md:justify-items-start {props.class}">
-	<h2 class="font-bold mb-4 text-base-content text-opacity-60">SOCIALS</h2>
 	<div class="text-opacity-60 text-base-content">Social icons placeholder</div>
 </div>
